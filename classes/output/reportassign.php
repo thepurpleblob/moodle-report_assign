thepurpleblob/moodle-report_assign

commit 79bdfdd2d490f46b453eb142d38339b8bccaebe2

Only show profile cell if profile fields are enabled.

templates/reportassign.mustache
@@ -48,9 +48,11 @@
         {{# submissions }}
             <tr class="group_0 {{# groupids }}group_{{ . }} {{/ groupids}}">
                 <td>{{ fullname }}</td>
-                {{# profiledata }}
-                    <td>{{ . }}</td>
-                {{/ profiledata }}
+                {{# profilefields }}
+                    {{# profiledata }}
+                        <td>{{ . }}</td>
+                    {{/ profiledata }}
+                {{/ profilefields }}
                 {{# groupselect }}
                     <td>{{ groups }}</td>
                 {{/ groupselect }}

commit 04b6ccb6859453b1f354f25c0a95b960113f6bcc

Fix duplicated profile fields.
Commit 79bdfdd introduced a bug where for every profile field enabled,
a copy of all enabled fields would be included. E.g. enable 3 fields,
get 9 fields in the output.

classes/output/reportassign.php
@@ -73,6 +73,7 @@ class reportassign implements renderable, templatable {
         $cm = get_coursemodule_from_instance('assign', $this->assignment->id);
         $groupmode = $cm->groupmode;
         $groups = groups_get_all_groups($this->course->id);
+        $profilefields = $this->get_profilefields();
 
         return [
             'canrevealnames' => has_capability('report/assign:shownames', $this->context) && $this->assignment->blindmarking,
@@ -86,7 +87,8 @@ class reportassign implements renderable, templatable {
             'urkundenabled' => \report_assign\lib::urkund_enabled($this->assignment->id),
             'groupselect' => $groupmode != 0,
             'groups' => array_values($groups),
-            'profilefields' => $this->get_profilefields(),
+            'profilefieldsenabled' => sizeof($profilefields),
+            'profilefields' => $profilefields,
         ];
     }
 

templates/reportassign.mustache
@@ -48,11 +48,11 @@
         {{# submissions }}
             <tr class="group_0 {{# groupids }}group_{{ . }} {{/ groupids}}">
                 <td>{{ fullname }}</td>
-                {{# profilefields }}
+                {{# profilefieldsenabled }}
                     {{# profiledata }}
                         <td>{{ . }}</td>
                     {{/ profiledata }}
-                {{/ profilefields }}
+                {{/ profilefieldsenabled }}
                 {{# groupselect }}
                     <td>{{ groups }}</td>
                 {{/ groupselect }}